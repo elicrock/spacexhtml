7a0aff50c847d34ae1ebf1c3c39bc82ab5430529
Paul <paul.ivanov1990@gmail.com>
2020-08-05 14:23:26 +0300

    Homework 2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const instance = new vidbg('.video', {
+  mp4: 'video/world.mp4',
+  webm: 'video/world.webm',
+  poster: 'video/poster.jpg',
+  overlay: false,
+  overlayColor: '#000',
+  overlayAlpha: 0.3
+}, {
+  
+});
+
+const rellax = new Rellax('.rocket');
